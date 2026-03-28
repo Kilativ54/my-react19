81bd979e7fe66c9585b8ea948f3b012a68ead074
Kilativ54 <vitalikechkalov@gmail.com>
2026-03-28 13:56:02 +0100

    working with components

---
+++ src/components/AppBar.jsx
@@ -0,0 +1,11 @@
+import Logo from "./Logo"
+
+export default function AppBar() {
+    return (<header>
+        <div>
+<Logo />
+            <p>Welcome</p>
+            <button>Log out</button>
+        </div>
+    </header>)
+}
