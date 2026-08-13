f7db61f276d11fc6189d829317cb9806e0787eda
Kilativ54 <vitalikechkalov@gmail.com>
2026-08-13 20:44:15 +0200

    add local img

--- src/components/PilotProfile/PilotProfile.jsx
+++ src/components/PilotProfile/PilotProfile.jsx
@@ -1,12 +1,15 @@
 import clsx from "clsx"; 
+import pilotAvataer from "../../assets/lion.jpg";
 import css from "./PilotProfile.module.css";
+
 
 export default function PilotProfile({ pilot: { name, rank, age, active } }) {
  
   const statusClsx = clsx (css.text, active ? css.active : css.retired)
- 
+ console.log(statusClsx);
   return (
     <div>
+      <img src={pilotAvataer} alt="photo" />
       <p className={css.text}>Name:{name}</p>
       <p className={css.text}>Rank: {rank}</p>
       <p className={css.text}>Age: {age}</p>
