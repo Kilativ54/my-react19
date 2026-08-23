c47e66a7d797e7b51765f04a5fb002774553e63c
Kilativ54 <vitalikechkalov@gmail.com>
2026-08-23 23:15:07 +0200

    using one state for diferent buttons

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,8 +1,18 @@
 import { useState } from "react";
-export default function Counter({initialValue = 0, text = "Click me"}) {
-    const [clicks, setClicks] = useState(initialValue);
-    const handleClickStateSame = () => {
-        setClicks( clicks + 1);
-    }
-    return <button onClick={handleClickStateSame}>{text}: { clicks }</button>
+
+// diferent state for different buttons
+
+// export default function Counter({ initialValue = 0, text = "Click me" }) {
+//     const [clicks, setClicks] = useState(initialValue);
+//     const handleClickStateSame = () => {
+//         setClicks( clicks + 1);
+//     }
+//     return <button onClick={handleClickStateSame}>{text}: { clicks }</button>
+// }
+
+// one state for different buttons
+
+export default function Counter({ value, onX, text = "Click me" }) {
+
+    return (<button onClick={onX}>{text}: { value }</button>)
 }
